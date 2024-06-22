85a314db60d4410e0cd9c0504f63bbd544d9e071
Rolando HR <rolando167@hotmail.com>
2024-06-22 20:52:33 +0200

    RH 22 - CA 🎱 🐢

---
+++ Propios/1.Abrir Programas.bat
@@ -0,0 +1,10 @@
+@echo off
+Start winrar.exe
+::Start "" "C:\Users\Rolando\AppData\Local\Postman\Postman.exe"
+Start cmd.exe
+Start cmd.exe
+
+Start C:\www\bat\curso\curso-batch\clases\data\info.txt
+
+::Abrir un programa ir a "propiedades" y copiar toda ruta ejemp "...\Postman.exe"
+exit
---
+++ Propios/2.Abrir vsCode.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "C:\Users\Computador\Desktop\Code A"
+start "" "C:\Users\Computador\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+exit
