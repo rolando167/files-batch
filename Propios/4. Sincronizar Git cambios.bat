aadb95c7668c1e5cad683599d77cba971ef6816c
Rolando HR <rolando167@hotmail.com>
2024-06-23 09:55:18 +0200

    RH 23 - ca

--- Propios/4. Sincronizar Git cambios.bat
+++ Propios/4. Sincronizar Git cambios.bat
@@ -3,12 +3,13 @@
 :: git config --list
 
 
-start "Git Configs" cmd /k "git config --list"^&color B0
+start "Git Configs ^_^!! |" cmd /k "git config --list"^&color B0
 
-set USERPROFILE=Rolando HR
-cd C:\www\bat\curso\curso-batch
-set HOME=%USERPROFILE%
-git pull origin
+::set USERPROFILE=Rolando HR
+::cd C:\www\bat\curso\curso-batch
+::set HOME=%USERPROFILE%
+::git pull origin
+
 pause
 
 ::git config --global user.email y*********@gmail.com
